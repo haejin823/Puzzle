1c2d1ca8078633a8b6899907653bf13b47d100e7
haejin823 <qr5czx2nyq@privaterelay.appleid.com>
2025-11-28 15:23:31 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'christmas-puzzle-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './christmas.jpg',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+// 설치 단계
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// 오래된 캐시 삭제
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+// fetch 시 캐시 우선 제공
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
